ff8d125abd23e52625ba185d06ecb2830bbdedb5
mayo0007 <muhammad.umar@thecherrybyte.com>
2024-06-13 04:59:51 +0500

    types added

---
+++ src/types/config.d.ts
@@ -0,0 +1,16 @@
+export interface S3BucketConnectionAttributes{
+    accessKeyId:string,
+    secretAccessKey:string,
+    region:string
+}
+export interface S3BucketObjectRetrivalAttributes{
+    Bucket:string, // This is the bucket name
+    Key:string // This is path to that specific object
+}
+export interface PinataConfigAttributes{
+    PinataToken:string,
+    pinata_api_key:string,
+    pinata_secret_api_key:string
+}
+export const PINATA_API_PINFILETOIPFS:string
+
